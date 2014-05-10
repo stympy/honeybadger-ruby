9b6e1b7f546163df0dd80bd3ffdae66b9bb0a86e
Benjamin Curtis <benjamin.curtis@gmail.com>
2014-05-10 10:27:33 -0700

    Don't instrument our own http requests

--- lib/honeybadger/integrations/net_http.rb
+++ lib/honeybadger/integrations/net_http.rb
@@ -2,6 +2,10 @@ class Net::HTTP
   def request_with_honeybadger(*args, &block)
     request = args[0]
     uri = request.path.match(%r{https?://}) ? URI(request.path) : URI("http#{use_ssl? ? 's' : ''}://#{address}:#{port}#{request.path}")
+    if uri.host.match("honeybadger.io")
+      return request_without_honeybadger(*args, &block)
+    end
+
     payload = { uri: uri, method: request.method }
     ActiveSupport::Notifications.instrument("net_http.request", payload) do
       request_without_honeybadger(*args, &block)
